dbfeb55ae4cecbd606d118b654d79f4a66d44b93
george1518 <guganraj.me@gmail.com>
2026-06-29 19:05:23 +0530

    add CI workflow and eslint

---
+++ frontend/eslint.config.mjs
@@ -0,0 +1,19 @@
+import js from "@eslint/js";
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: {
+      globals: globals.browser,
+    },
+    plugins: {
+      react: pluginReact,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...pluginReact.configs.recommended.rules,
+    },
+  },
+];
