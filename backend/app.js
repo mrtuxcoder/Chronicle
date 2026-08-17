d16cf18d68bb253545332fca9723b5229527cb44
mrtuxcoder <guganrajrengaraju@gmail.com>
2026-08-17 16:32:12 +0530

    test-modify: backend

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+console.log("Chronicle API is running...");
 
 // CORS middleware
 app.use((req, res, next) => {
